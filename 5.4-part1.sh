ed8b06e7abbb624ca6a80baf556f910b2e0d5568
eay <1015714710@qq.com>
2023-05-07 17:49:32 +0800

    换源

--- 5.4-part1.sh
+++ 5.4-part1.sh
@@ -2,9 +2,9 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-# sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git small-package https://github.com/kenzok8/small-package' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '$a  src-git kenzok8 https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
+# sed -i '$a  src-git kenzok8 https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
 # sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
 
