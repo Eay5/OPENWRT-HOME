5905fba88cc0df51965d29e26ef916d08835748f
eay <1015714710@qq.com>
2023-05-07 11:18:05 +0800

    passwall

--- 5.10-part1.sh
+++ 5.10-part1.sh
@@ -1,23 +1,3 @@
-#!/bin/bash
-###
- # @Author: eay
- # @Date: 2022-01-13 17:09:14
- # @LastEditors: eay 1015714710@qq.com
- # @Autor: Seven
- # @LastEditTime: 2023-03-29 14:17:14
- # @Description: 
-### 
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
@@ -25,7 +5,7 @@
 # sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
 sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
 # sed -i '$a  src-git kenzok8 https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
-# sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
 
 #sed -i '$a src-git darkmatter https://github.com/apollo-ng/luci-theme-darkmatter.git' feeds.conf.default
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argonnew
