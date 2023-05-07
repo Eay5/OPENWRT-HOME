ddfa543c8be6c5d004c8e7148fd8cb91fe2361cb
eay <1015714710@qq.com>
2023-05-07 20:35:47 +0800

    换主题

--- 5.19-part1.sh
+++ 5.19-part1.sh
@@ -9,7 +9,7 @@ sed -i '$a src-git smallpasswall https://github.com/kenzok8/small' feeds.conf.de
 # sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
 
 #sed -i '$a src-git darkmatter https://github.com/apollo-ng/luci-theme-darkmatter.git' feeds.conf.default
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argonnew
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argonnew
 
 #ddns-go
 git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
