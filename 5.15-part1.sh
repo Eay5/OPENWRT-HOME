a8c1a4cb5b2406403d6e8e4c403069b1cbad2a8a
eay <1015714710@qq.com>
2023-05-16 16:44:32 +0800

    换源

--- 5.15-part1.sh
+++ 5.15-part1.sh
@@ -2,11 +2,11 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 #new
-#sed -i '$a src-git smallpackage https://github.com/kenzok8/small-package' feeds.conf.default
-# sed -i '$a src-git smallpasswall https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git smallpackage https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git smallpasswall https://github.com/kenzok8/small' feeds.conf.default
 #stable
-sed -i '$a  src-git kenzok8 https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
-sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
+# sed -i '$a  src-git kenzok8 https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
+# sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
 
 # sed -i '$a src-git darkmatter https://github.com/apollo-ng/luci-theme-darkmatter.git' feeds.conf.default
 # git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argonenew
