e9339d518afd89935bd977719bc1e668f455c4ce
eay <1015714710@qq.com>
2023-07-19 08:57:53 +0800

    修复编译

--- 5.15-part1.sh
+++ 5.15-part1.sh
@@ -6,9 +6,9 @@
 # sed -i '$a src-git smallpasswall https://github.com/kenzok8/small' feeds.conf.default
 # #stable
 # sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
-# sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
 sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
 # # sed -i '$a src-git darkmatter https://github.com/apollo-ng/luci-theme-darkmatter.git' feeds.conf.default
 # # git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argonenew
 # #ddns-go
