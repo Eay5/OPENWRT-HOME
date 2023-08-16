324c20fc22abd8a7fdb044fe52259aaae3f5b5d0
eay <1015714710@qq.com>
2023-08-16 22:43:40 +0800

    改源测试

--- 5.4-part2.sh
+++ 5.4-part2.sh
@@ -28,6 +28,6 @@ sed -i 's/KERNEL_PATCHVER:=*.*/KERNEL_PATCHVER:=5.4/g' ./target/linux/x86/Makefi
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' ./package/lean/default-settings/files/zzz-default-settings
 #4.an-theme
 #取消bootstrap为默认主题：
-sed -i 's/luci-theme-bootstrap/luci-theme-argon-1806/g' ./feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argone/g' ./feeds/luci/collections/luci/Makefile
 #name
 sed -i "s/hostname='OpenWrt'/hostname='OpenWrt-eay'/g" ./package/base-files/files/bin/config_generate
