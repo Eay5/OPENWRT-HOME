3465e5daef89b39d3309858657977cc61704e62d
eay <1015714710@qq.com>
2023-09-08 08:01:37 +0800

    passwall添加插件

--- 5.4-part2.sh
+++ 5.4-part2.sh
@@ -17,9 +17,8 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-#主题
- 
- 
+#等待时间
+sed -i 's/default "5"/default "0"/g' config/Config-images.in
 #ip
 sed -i 's/192.168.1.1/10.10.10.133/g' ./package/base-files/files/bin/config_generate
 # 编译5.10
