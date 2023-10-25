9c08e21193cd10930a357c6b4ca06cf674aed134
eay <1015714710@qq.com>
2023-10-25 22:12:08 +0800

    稳定源

--- 5.19-part1.sh
+++ 5.19-part1.sh
@@ -2,8 +2,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 
-sed -i '$a  src-git small8 https://github.com/kenzok8/small-package.git' feeds.conf.default
-# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '$a  src-git small8 https://github.com/kenzok8/small-package.git' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
