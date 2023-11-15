50f237864c85d84dc219f21eaec06f5283d71643
sinoitc <1015714710@qq.com>
2023-11-15 16:20:54 +0800

    升级golang

--- 5.15-part1.sh
+++ 5.15-part1.sh
@@ -1,16 +1,17 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-
-# sed -i '$a  src-git small8 https://github.com/kenzok8/small-package.git' feeds.conf.default
+#new
+#sed -i '$a  src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 # git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
 # # sed -i '$a src-git darkmatter https://github.com/apollo-ng/luci-theme-darkmatter.git' feeds.conf.default
 # # git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argonenew
 # #ddns-go
+pushd feeds/packages/lang
+rm -rf golang && svn co https://github.com/openwrt/packages/branches/openwrt-23.05/lang/golang
+popd
 git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
 # #smartdns
 # #git clone https://github.com/pymumu/smartdns.git package/smartdns
