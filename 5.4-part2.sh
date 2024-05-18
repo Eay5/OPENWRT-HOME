97ada93db1ebcb2c017bb1d107866383bc0ba874
eay <1015714710@qq.com>
2024-05-18 19:01:28 +0800

    测试

--- 5.4-part2.sh
+++ 5.4-part2.sh
@@ -25,9 +25,9 @@ sed -i 's/192.168.1.1/10.10.10.133/g' ./package/base-files/files/bin/config_gene
 # 编译5.10
 sed -i 's/KERNEL_PATCHVER:=*.*/KERNEL_PATCHVER:=5.4/g' ./target/linux/x86/Makefile
 #2. Clear the login password
-# sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' ./package/lean/default-settings/files/zzz-default-settings
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' ./package/lean/default-settings/files/zzz-default-settings
 #4.an-theme
 #取消bootstrap为默认主题：
-sed -i 's/luci-theme-bootstrap/luci-theme-argon-2102/g' ./feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argone/g' ./feeds/luci/collections/luci/Makefile
 #name
 sed -i "s/hostname='OpenWrt'/hostname='EAY'/g" ./package/base-files/files/bin/config_generate
