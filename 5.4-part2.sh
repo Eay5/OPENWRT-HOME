77b5d2aa4efcac59353ce1850cbd8b50af779278
eay <1015714710@qq.com>
2024-05-18 15:24:13 +0800

    更改设置

--- 5.4-part2.sh
+++ 5.4-part2.sh
@@ -25,7 +25,7 @@ sed -i 's/192.168.1.1/10.10.10.133/g' ./package/base-files/files/bin/config_gene
 # 编译5.10
 sed -i 's/KERNEL_PATCHVER:=*.*/KERNEL_PATCHVER:=5.4/g' ./target/linux/x86/Makefile
 #2. Clear the login password
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' ./package/lean/default-settings/files/zzz-default-settings
+# sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' ./package/lean/default-settings/files/zzz-default-settings
 #4.an-theme
 #取消bootstrap为默认主题：
 sed -i 's/luci-theme-bootstrap/luci-theme-argon-2102/g' ./feeds/luci/collections/luci/Makefile
