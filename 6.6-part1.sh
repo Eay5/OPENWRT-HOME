661abea644846ae80e72e2e698d0e014b1d3ea85
eay <1015714710@qq.com>
2024-05-18 20:26:32 +0800

    修复BUG

--- 6.6-part1.sh
+++ 6.6-part1.sh
@@ -2,9 +2,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 #new
-#sed -i '$a  src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '2i src-git small https://github.com/Eay5/small.git' feeds.conf.default
 ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/lang/golang
