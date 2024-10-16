3768024403684b6f2c281a03ed6c836e537cfbff
eay <1015714710@qq.com>
2024-10-16 11:47:52 +0800

    turboacc测试

--- 5.4-part1.sh
+++ 5.4-part1.sh
@@ -2,10 +2,11 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 #new
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/Eay5/eay-packages.git' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small.git' feeds.conf.default
 # sed -i '2i src-git small https://github.com/Eay5/small.git' feeds.conf.default
 ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/luci/applications/luci-app-turboacc
 rm -rf feeds/packages/net/{alist,adguardhome,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
@@ -15,7 +16,7 @@ git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 # # git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argonenew
 # #ddns-go
 # pushd feeds/packages/lang
-git clone https://github.com/chenmozhijin/turboacc.git feeds/luci/applications/luci-app-turboacc
+ 
 # #smartdns
 # #git clone https://github.com/pymumu/smartdns.git package/smartdns
 # #git clone https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
