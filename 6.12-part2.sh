7c66585985211bab0727b9609c705a6c08f5ca12
eay <1015714710@qq.com>
2024-11-21 14:40:23 +0800

    添加6.12内核

--- 6.12-part2.sh
+++ 6.12-part2.sh
@@ -4,7 +4,7 @@
  # @Date: 2022-01-13 14:44:27
  # @LastEditors: eay 1015714710@qq.com
  # @Autor: Seven
- # @LastEditTime: 2022-07-13 06:23:42
+ # @LastEditTime: 2023-05-16 23:35:35
  # @Description: 
 ### 
 #
@@ -19,16 +19,15 @@
 #
  
 #ip
-sed -i 's/192.168.1.1/192.168.0.133/g' ./package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.0.133/g' openwrt/package/base-files/files/bin/config_generate
 # 编译5.10
-sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.19/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=*.*/KERNEL_PATCHVER:=6.12/g' ./target/linux/x86/Makefile
 #2. Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' ./package/lean/default-settings/files/zzz-default-settings
 #4.an-theme
 #取消bootstrap为默认主题：
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon-jerrykuku/g' ./feeds/luci/collections/luci/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' ./feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
 #name
-sed -i "s/hostname='OpenWrt'/hostname='OpenWrt-eay'/g" ./package/base-files/files/bin/config_generate
+sed -i "s/hostname='OpenWrt'/hostname='EAY'/g" ./package/base-files/files/bin/config_generate
 
  
