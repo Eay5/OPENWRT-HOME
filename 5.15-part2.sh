6e6140a73d359e4135ddfe24896f97825e219dee
eay <1015714710@qq.com>
2024-12-04 12:37:56 +0800

    优化编译

--- 5.15-part2.sh
+++ 5.15-part2.sh
@@ -19,14 +19,14 @@
 #
  
 #ip
-sed -i 's/192.168.1.1/192.168.0.133/g' ./package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.0.133/g' package/base-files/files/bin/config_generate
 # 编译5.10
-sed -i 's/KERNEL_PATCHVER:=*.*/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=*.*/KERNEL_PATCHVER:=5.15/g' target/linux/x86/Makefile
 #2. Clear the login password
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' ./package/lean/default-settings/files/zzz-default-settings
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
 #4.an-theme
 #取消bootstrap为默认主题：
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 #name
-sed -i "s/hostname='LEDE'/hostname='EAY'/g" ./package/base-files/files/bin/config_generate
+sed -i "s/hostname='LEDE'/hostname='EAY'/g" package/base-files/files/bin/config_generate
  
