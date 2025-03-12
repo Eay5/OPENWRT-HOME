2c774ba5366a28667e4c7fedc171fa4fd8a260cf
eay <1015714710@qq.com>
2025-03-12 15:24:12 +0800

    修复配置文件错误

--- 6.12-part1.sh
+++ 6.12-part1.sh
@@ -13,7 +13,7 @@ rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftabl
 # # sed -i '$a src-git darkmatter https://github.com/apollo-ng/luci-theme-darkmatter.git' feeds.conf.default
 # # git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argonenew
 # #ddns-go
-pushd feeds/packages/lang
+# pushd feeds/packages/lang
 # git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
 # #smartdns
 # #git clone https://github.com/pymumu/smartdns.git package/smartdns
