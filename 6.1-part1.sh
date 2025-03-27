b2db077461db381f4d1e5a9989f45a7da30e3008
eay <1015714710@qq.com>
2025-03-28 07:21:42 +0800

    修复go缺少的问题

--- 6.1-part1.sh
+++ 6.1-part1.sh
@@ -5,3 +5,5 @@ rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftabl
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+./scripts/feeds install -a 
