213940da19dcd15c27b5ea8138b8b007d61a994b
eay <1015714710@qq.com>
2025-04-01 15:30:04 +0800

    测试6.12

--- 6.12-part2.sh
+++ 6.12-part2.sh
@@ -1,5 +1,5 @@
 #ip
-sed -i 's/192\.168\.1\.1/192.168.0.133/g' package/lean/base-files/files/etc/config/network
+# sed -i 's/192\.168\.1\.1/192.168.0.133/g' package/lean/base-files/files/etc/config/network
 # 编译5.10
 sed -i 's/KERNEL_PATCHVER:=*.*/KERNEL_PATCHVER:=6.12/g' target/linux/x86/Makefile
 #2. Clear the login password
@@ -8,6 +8,6 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/f
 #取消bootstrap为默认主题：
 sed -i 's/luci-theme-bootstrap/luci-theme-argone/g' feeds/luci/collections/luci/Makefile
 #name
-sed -i "/option hostname/s/'LEDE'/'EAY'/" package/lean/base-files/files/etc/config/system
+# sed -i "/option hostname/s/'LEDE'/'EAY'/" package/lean/base-files/files/etc/config/system
 
  
