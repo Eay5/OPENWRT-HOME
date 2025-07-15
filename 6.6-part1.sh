efac95038528eb4e1266c6389d5dbdb089f62d54
eay <1015714710@qq.com>
2025-07-15 11:04:08 +0800

    修改配置例如GO升级版本

--- 6.6-part1.sh
+++ 6.6-part1.sh
@@ -1,5 +1,7 @@
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+# 删除不需要的包，但保留 v2ray 和 xray (SSR Plus+ 需要)
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,smartdns}
 rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/packages/lang/golang
