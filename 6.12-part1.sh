c2b38cb418726317ab3523d69258292182b7f001
eay <1015714710@qq.com>
2025-09-03 15:40:24 +0800

    优化编译

--- 6.12-part1.sh
+++ 6.12-part1.sh
@@ -6,6 +6,21 @@ rm -rf feeds/packages/net/{alist,adguardhome,mosdns}
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
 
+# 升级系统Go版本到1.25.0以支持xray-core编译
+echo "正在升级Go版本到1.25.0..."
+cd /tmp
+wget -q https://go.dev/dl/go1.25.0.linux-amd64.tar.gz
+echo "2852af0cb20a13139b3448992e69b868e50ed0f8a1e5940ee1de9e19a123b613  go1.25.0.linux-amd64.tar.gz" | sha256sum -c
+if [ $? -eq 0 ]; then
+    sudo rm -rf /usr/local/go
+    sudo tar -C /usr/local -xzf go1.25.0.linux-amd64.tar.gz
+    echo "Go 1.25.0 安装完成"
+else
+    echo "错误：Go包校验失败，请检查下载"
+    exit 1
+fi
+cd - > /dev/null
+
 # 添加自定义 Golang 包以解决依赖问题
 git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
 
