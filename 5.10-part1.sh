c44f0ebb08bbd87384db689ad8900101fba559a2
eay <1015714710@qq.com>
2025-09-03 10:23:31 +0800

    修复编译

--- 5.10-part1.sh
+++ 5.10-part1.sh
@@ -2,12 +2,12 @@ sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
 # 删除不需要的包，但保留 v2ray 和 xray (SSR Plus+ 需要)
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,smartdns}
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns}
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
 
 # 添加自定义 Golang 包以解决依赖问题
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 
 # 确保 SSR Plus+ 核心组件可用
 ./scripts/feeds install -a -p small
--- 6.1-part1.sh
+++ 6.1-part1.sh
@@ -2,7 +2,7 @@ sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
 # 删除不需要的包，但保留 v2ray 和 xray (SSR Plus+ 需要)
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,smartdns}
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns}
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
 
