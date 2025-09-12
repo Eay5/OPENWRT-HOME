905b103b9b11367c20ca7a341fddf55f77ac7e0a
eay <1015714710@qq.com>
2025-09-12 10:48:43 +0800

    修复6.12编译

--- 6.12-part1.sh
+++ 6.12-part1.sh
@@ -4,12 +4,12 @@ sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
 
 # 删除重复包以防止插件冲突（按官方建议）
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,smartdns}
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
 
-# 使用 sbwml 的 golang 仓库（24.x，兼容 xray-core）
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+# 使用 kenzok8 的 golang 仓库（官方推荐，支持新版 Sing-box 和 hysteria）
+git clone https://github.com/kenzok8/golang -b 1.25 feeds/packages/lang/golang
 
 # 安装所有 feeds
 ./scripts/feeds install -a
--- 6.12-part2.sh
+++ 6.12-part2.sh
@@ -20,7 +20,7 @@
  
 #ip
 sed -i 's/192\.168\.1\.1/192.168.0.133/g' package/base-files/files/bin/config_generate
-# 编译5.10
+# 使用6.12内核
 sed -i 's/KERNEL_PATCHVER:=*.*/KERNEL_PATCHVER:=6.12/g' target/linux/x86/Makefile
 #2. Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
@@ -28,6 +28,6 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/f
 #取消bootstrap为默认主题：
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 #name
-sed "s/hostname='LEDE'/hostname='EAY'/g" package/base-files/files/bin/config_generate
+sed -i "s/hostname='LEDE'/hostname='EAY'/g" package/base-files/files/bin/config_generate
 
  
