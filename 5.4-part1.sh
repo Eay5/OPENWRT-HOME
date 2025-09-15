72641a5e56f342c88e87d216e6882bf4314ad504
eay <1015714710@qq.com>
2025-09-15 14:16:29 +0800

    修复编译BUG

--- 5.4-part1.sh
+++ 5.4-part1.sh
@@ -3,3 +3,5 @@ sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang -b 1.25 feeds/packages/lang/golang
