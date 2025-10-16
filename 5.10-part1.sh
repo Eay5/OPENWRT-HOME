28911ee9b20152b58c2b9426eb97033fcca7fdd5
eay <1015714710@qq.com>
2025-10-16 14:19:37 +0800

    修复5.10

---
+++ 5.10-part1.sh
@@ -0,0 +1,11 @@
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/packages/lang/golang
+ 
+
+# 添加自定义 Golang 包以解决依赖问题
+git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
+./scripts/feeds install -a 
