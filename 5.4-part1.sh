339bcfeb37c884a7bd00b1a81cb7ead78fef3e85
eay <1015714710@qq.com>
2025-10-16 10:38:53 +0800

    修复

--- 5.4-part1.sh
+++ 5.4-part1.sh
@@ -8,16 +8,4 @@ rm -rf feeds/packages/lang/golang
 
 # 添加自定义 Golang 包以解决依赖问题
 git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
-
-# 确保 SSR Plus+ 核心组件可用
-./scripts/feeds install -a -p small
-./scripts/feeds install -a -p kenzo
-
-# 安装 SSR Plus+ 必需的核心包
-./scripts/feeds install -p packages shadowsocks-libev
-./scripts/feeds install -p small shadowsocksr-libev
-./scripts/feeds install -p small v2ray-core
-./scripts/feeds install -p small xray-core
-./scripts/feeds install -p small trojan-plus
-./scripts/feeds install -p small trojan-go
-./scripts/feeds install -p small naiveproxy
+./scripts/feeds install -a 
