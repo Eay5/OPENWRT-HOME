ca23d57c03378ecf94f1fb7cdf1a8e6b48ea3f9c
eay <1015714710@qq.com>
2025-10-17 09:17:28 +0800

    优化

--- 5.10-part1.sh
+++ 5.10-part1.sh
@@ -4,7 +4,6 @@ sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
- 
 
 # 添加自定义 Golang 包以解决依赖问题
 git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
